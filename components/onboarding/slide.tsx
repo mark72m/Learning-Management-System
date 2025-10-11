24634deb2c543eb90a72fdda35a6db303b2553a0
markm <markmutisya72@gmail.com>
2025-10-11 10:54:16 -0700

    Next Button

--- components/onboarding/slide.tsx
+++ components/onboarding/slide.tsx
@@ -1,7 +1,8 @@
 import { HEIGHT, WIDTH } from '@/configs/constants';
-import { fontSizes, SCREEN_WIDTH } from '@/themes/app.constants';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { fontSizes, SCREEN_WIDTH, windowHeight, windowWidth } from '@/themes/app.constants';
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { scale, verticalScale } from 'react-native-size-matters';
 import Svg, { Defs, RadialGradient, Rect, Stop } from 'react-native-svg';
 
@@ -11,6 +12,17 @@ export default function Slide({ slide, index, setIndex, totalSlides }: {
   setIndex: (value: number) => void;
   totalSlides: number;
 }) {
+
+  const [ModalVisible, setModalVisible] = useState(false);
+  const handlePress = ( 
+    index: number, setIndex: (index: number) => void ) => {
+    if(index === 2){
+      setModalVisible(true);
+    } else {
+      setIndex(index + 1);
+    } 
+  } 
+  
   return (
     <>
       <Svg style={StyleSheet.absoluteFill}>
@@ -38,31 +50,60 @@ export default function Slide({ slide, index, setIndex, totalSlides }: {
                 color: "#05030D",
                 fontFamily: "Poppins-600SemiBold",
               }}>
-                {slide.title}
-              </Text>
+              {slide.title}
+            </Text>
 
-              <Text
+            <Text
               style={{
                 fontSize: fontSizes.FONT30,
                 fontWeight: '600',
                 color: "#05030D",
                 fontFamily: "Poppins-600SemiBold",
               }}>
-                {slide.secondTitle}
-              </Text>
+              {slide.secondTitle}
+            </Text>
 
-              <Text
+            <Text
               style={{
                 paddingVertical: verticalScale(4),
                 fontSize: fontSizes.FONT18,
                 color: "#05030D",
                 fontFamily: "Poppins-300Light",
               }}>
-                {slide.subtitle}
-              </Text>
+              {slide.subtitle}
+            </Text>
           </View>
         </View>
       </View>
+
+      <View style={styles.indicatorContainer}>
+        {Array.from({ length: totalSlides }).map((_, i) => (
+          <TouchableOpacity
+            key={i}
+            style={[styles.indicatorContainer, i === index && styles.activeIndicator]} />
+        ))}
+      </View>
+
+      {/* Next Button */}
+      {index <= totalSlides - 1 && (
+        <LinearGradient
+          colors={["#6D55FE", "#8976FC"]}
+          style={styles.nextButton}>
+
+          <Pressable
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              height: "100%",
+            }}
+            onPress={() => handlePress(index, setIndex)}
+            >
+            <Text style={styles.nextButtonText}>Next</Text>
+          </Pressable>
+        </LinearGradient>
+      )}
 
 
     </>
@@ -75,5 +116,48 @@ const styles = StyleSheet.create({
     padding: scale(60),
     paddingTop: verticalScale(100),
     alignItems: 'center',
-  }
+  },
+
+  indicatorContainer: {
+    flexDirection: 'row',
+    marginTop: verticalScale(35),
+    position: 'absolute',
+    bottom: verticalScale(55),
+    left: scale(22),
+
+  },
+
+  indicator: {
+    height: verticalScale(7),
+    width: scale(18),
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    marginHorizontal: scale(4),
+    borderRadius: scale(4),
+  },
+
+  activeIndicator: {
+    height: verticalScale(8),
+    width: scale(40),
+    backgroundColor: "white"
+  },
+
+  nextButton: {
+    position: "absolute",
+    zIndex: 999999999,
+    right: windowWidth(25),
+    bottom: windowHeight(50),
+    marginTop: windowHeight(30),
+    alignItems: "center",
+    justifyContent: "center",
+    width: windowWidth(140),
+    height: windowHeight(37),
+    borderRadius: windowWidth(20),
+
+  },
+
+  nextButtonText: {
+    color: "white",
+    fontSize: fontSizes.FONT22,
+    fontWeight: "bold"
+  },
 })
